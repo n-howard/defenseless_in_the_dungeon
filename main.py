bc4d60f8d87d44a3d3901bd4273abba73b84ef6a
n-howard <nhoward@oberlin.edu>
2025-10-22 17:03:22 -0500

    Player Class and Enemy Level

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Hello world")
+import random
+
+class Player:
+    xpHad = 0
+    playerLevel = 1
+    def __init__(self):
+        return
+    def levelUpCheck(self, xpGained):
+        if xpNeeded(self.playerLevel) <= xpGained + self.xpHad:
+            self.playerLevel+=1
+            return True
+        return False
+ 
+def xpNeeded(level):
+    return (level * someVariable) # set someVariable equal to something and/or write and equation
+    
+def enemyLevel(playerLevel):
+    enemyLevel = playerLevel + random.randint(-5, 5)
+    if enemyLevel < 1:
+        enemyLevel = 1
+    return enemyLevel
+
+player = Player()
+
+
+
+
+
+
+
+
